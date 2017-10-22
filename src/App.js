80f128d5af03bf122dbe66c4d0686c2c58d40339
hounslow <hounslowmatthew@gmail.com>
2017-10-22 12:39:10 -0700

    Ejected app, creating Map component

--- src/App.js
+++ src/App.js
@@ -2,35 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import request from 'superagent';
+import Map from './Map.js';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-    };
-    this.getBusPoints = this.getBusPoints.bind(this);
-  }
-
-  getBusPoints() {
-    request
-    .get('/api')
-    .set('accept', 'application/json')
-    .end((err, res) => {
-      if (err) throw err;
-    });
-  }
-
   render() {
     this.getBusPoints();
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Map />
       </div>
     );
   }
